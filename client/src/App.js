24f6e31c52d41658634c07fdd693f16330794bc1
Joshua Taguicana <joshuataguicana@Joshuas-MacBook-Pro.local>
2019-09-21 14:50:31 -0400

    completed navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // pages
 import HomePage from './pages/HomePage/HomePage';
 
+// Nav Bar
+import Navbar from "./components/navbar"
+
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
       </Switch>
+      <Navbar />
     </BrowserRouter>
   );
 }
