6e30395b966fd141c54f8ef1dcc463116edc9602
Joshua Taguicana <joshtaguicana@gmail.com>
2019-09-21 19:17:35 -0400

    added assets

--- client/src/components/ItemList/ItemList.js
+++ client/src/components/ItemList/ItemList.js
@@ -8,25 +8,7 @@ export class ItemList extends Component {
         <h2 className="text-base mb-4">All Items</h2>
         <ul className="mb-24">
           <li className="h-24 mb-4 relative item-card">
-            <p className="absolute bottom-0 pb-1 pl-2 text-white">Brocolli</p>
-          </li>
-          <li className="h-24 mb-4 relative item-card">
-            <p className="absolute bottom-0 pb-1 pl-2 text-white">Potatoes</p>
-          </li>
-          <li className="h-24 mb-4 relative item-card">
-            <p className="absolute bottom-0 pb-1 pl-2 text-white">Wasbi Peas</p>
-          </li>
-          <li className="h-24 mb-4 relative item-card">
-            <p className="absolute bottom-0 pb-1 pl-2 text-white">Bananas</p>
-          </li>
-          <li className="h-24 mb-4 relative item-card">
-            <p className="absolute bottom-0 pb-1 pl-2 text-white">Durians</p>
-          </li>
-          <li className="h-24 mb-4 relative item-card">
-            <p className="absolute bottom-0 pb-1 pl-2 text-white">Brocolli</p>
-          </li>
-          <li className="h-24 mb-4 relative item-card">
-            <p className="absolute bottom-0 pb-1 pl-2 text-white">Potatoes</p>
+            <p className="absolute bottom-0 pb-1 pl-2 text-white">Red Peppers</p>
           </li>
         </ul>
       </div>
