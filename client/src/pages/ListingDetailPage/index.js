86dfca51b95c93fede915d6651fecf7f298f9656
Yash Gopal <reach.yashg@gmail.com>
2019-09-22 02:47:11 -0400

    rf listdetailpage

--- client/src/pages/ListingDetailPage/index.js
+++ client/src/pages/ListingDetailPage/index.js
@@ -4,28 +4,19 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import AvatarCircle from '../../components/AvatarCircle';
 import AddToCartCard from '../../components/AddToCartCard';
+import InfoLine from '../../components/InfoLine';
 
 const Hero = styled.div`
   flex: 0 1 298px;
 `;
-
 const Navigation = styled.div`
   flex: 1 0 auto;
 `;
-
 const ListingTitle = styled.h1``;
-const ListingPriceQuantity = styled.div``;
-const ListingPrice = styled.h2``;
-const ListingQuantity = styled.h2``;
-
-const SellerDetails = styled.div`
-`;
-
-const ListingDetails = styled.div`
-`;
-
-const ProfileLine = styled.div`
-`;
+const SellerDetails = styled.div``;
+const ListingDetails = styled.div``;
+const ExchangeDetails = styled.div``;
+const ProfileLine = styled.div``;
 
 export default class ListingDetailPage extends Component {
   state = {
@@ -45,25 +36,45 @@ export default class ListingDetailPage extends Component {
     return (
       <section className={`${this.state.base} min-h-screen flex flex-col`}>
         <Hero className="bg-gray-700 text-gray-100 flex flex-col">
-          <Navigation className="p-4 h-3">
-            <Link to="/">&lt; Back</Link>
+          <Navigation className="p-4 flex-auto">
+            <Link to="/buy">&lt; Back</Link>
           </Navigation>
-          <div className="px-4 flex justify-between items-center">
+          <div className="px-4 my-3">
+            <ListingTitle className="text-4xl text-gray-100 h-full w-2/4 inline-block">Broccoli</ListingTitle>
+            <div className="text-2xl h-full w-2/4 inline-block text-right">$0.56/lb</div>
+          </div>
+          {/* <div className="px-4 flex justify-between items-center">
             <ListingTitle className="text-4xl text-gray-100 h-full">Broccoli</ListingTitle>
             <div className="text-2xl h-full">$0.56/lb</div>
           </div>
           <ListingPriceQuantity className="flex justify-between px-4 items-center mb-4">
             <ListingPrice className="text-2xl">$1.24/lb</ListingPrice>
             <ListingQuantity className="text-gray-500">10lb available</ListingQuantity>
-          </ListingPriceQuantity>
+          </ListingPriceQuantity> */}
         </Hero>
-        <SellerDetails className="p-4">
-          <ProfileLine className="py-4 flex items-center">
-            <AvatarCircle></AvatarCircle>
-            <span>Food company Inc.</span>
-          </ProfileLine>
-        </SellerDetails>
-        <ListingDetails className="p-4">
+        <ListingDetails className="px-4">
+          <ExchangeDetails>
+            {/* TODO: */}
+          </ExchangeDetails>
+          <SellerDetails>
+            <InfoLine title="supplier">
+              <ProfileLine className="py-4 flex items-center">
+                <AvatarCircle></AvatarCircle>
+                <span>Food company Inc.</span>
+              </ProfileLine>
+            </InfoLine>
+            <InfoLine title="location">
+              123 Sesame Street
+              Toronto ON
+              1A1 B2B
+            </InfoLine>
+            <InfoLine title="expires in">
+              2 Days
+            </InfoLine>
+            <InfoLine title="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam enim tellus eu suspendisse molestie ullamcorper ac. Risus, non habitasse molestie bibendum. Sit enim magna sollicitudin vel. Metus, enim, et convallis justo, vivamus sit sed.
+            </InfoLine>
+          </SellerDetails>
           {this.state.listing.desc}
         </ListingDetails>
         <AddToCartCard>
