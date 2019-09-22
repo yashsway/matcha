6de220aeeac7d3a9f60b6157afdfffbbc251c02a
Yash Gopal <reach.yashg@gmail.com>
2019-09-22 04:45:41 -0400

    new item flow complete

--- client/src/components/TextInput/index.js
+++ client/src/components/TextInput/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default (props) => {
   return (
     <>
-      <input type={props.type || 'text'} name={props.name || ''} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2" />
+      <input type={props.type || 'text'} name={props.name || ''} placeholder={props.placeholder || ''} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2" />
     </>
   )
 }
