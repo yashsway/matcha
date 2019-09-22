45c1044e5389df5c0816146b8fbf25a880a4df5f
Joshua Taguicana <joshtaguicana@gmail.com>
2019-09-21 20:09:15 -0400

    completed buy side hompage

--- client/src/components/ItemList/ItemList.js
+++ client/src/components/ItemList/ItemList.js
@@ -5,10 +5,63 @@ export class ItemList extends Component {
   render() {
     return (
       <div className="mt-4">
-        <h2 className="text-base mb-4">All Items</h2>
+        <div className="flex flex-row justify-between">
+          <h2 className="text-base mb-4">All Items</h2>
+          <p className="text-xs text-gray-600">Newest to oldest</p>
+        </div>
+        <ul className="mb-2">
+          <li className="h-32 mb-4 relative item-card rounded red-peppers-item">
+            <div className="absolute bottom-0 text-white pb-1 pl-2 w-full">
+              <div className="flex flex-row justify-between mb-4">
+                <p className="text-white">Red Peppers</p>
+                <p className="text-white pr-1">$0.56/lb</p>
+              </div>
+              <p className="absolute bottom-0 text-xs mb-1">Honeyfield Farms</p>
+            </div>
+          </li>
+        </ul>
+        <ul className="mb-2">
+          <li className="h-32 mb-4 relative item-card rounded potatoes-item">
+            <div className="absolute bottom-0 text-white pb-1 pl-2 w-full">
+              <div className="flex flex-row justify-between mb-4">
+                <p className="text-white">Potatoes</p>
+                <p className="text-white pr-1">$0.56/lb</p>
+              </div>
+              <p className="absolute bottom-0 text-xs mb-1">Honeyfield Farms</p>
+            </div>
+          </li>
+        </ul>
+        <ul className="mb-2">
+          <li className="h-32 mb-4 relative item-card rounded carrots-item">
+            <div className="absolute bottom-0 text-white pb-1 pl-2 w-full">
+              <div className="flex flex-row justify-between mb-4">
+                <p className="text-white">Carrots</p>
+                <p className="text-white pr-1">$0.56/lb</p>
+              </div>
+              <p className="absolute bottom-0 text-xs mb-1">Honeyfield Farms</p>
+            </div>
+          </li>
+        </ul>
+        <ul className="mb-2">
+          <li className="h-32 mb-4 relative item-card rounded bok-choy-item">
+            <div className="absolute bottom-0 text-white pb-1 pl-2 w-full">
+              <div className="flex flex-row justify-between mb-4">
+                <p className="text-white">Bok Choy</p>
+                <p className="text-white pr-1">$0.56/lb</p>
+              </div>
+              <p className="absolute bottom-0 text-xs mb-1">Honeyfield Farms</p>
+            </div>
+          </li>
+        </ul>
         <ul className="mb-24">
-          <li className="h-24 mb-4 relative item-card">
-            <p className="absolute bottom-0 pb-1 pl-2 text-white">Red Peppers</p>
+          <li className="h-32 mb-4 relative item-card rounded brocolli-item">
+            <div className="absolute bottom-0 text-white pb-1 pl-2 w-full">
+              <div className="flex flex-row justify-between mb-4">
+                <p className="text-white">brocolli</p>
+                <p className="text-white pr-1">$0.56/lb</p>
+              </div>
+              <p className="absolute bottom-0 text-xs mb-1">Honeyfield Farms</p>
+            </div>
           </li>
         </ul>
       </div>
--- client/src/components/Recommended/Recommended.js
+++ client/src/components/Recommended/Recommended.js
@@ -5,13 +5,13 @@ export class Recommended extends Component {
   render() {
     return (
       <div className="mt-4">
-        <h2 className="text-xl font-bold ">Marketplace</h2>
+        <h2 className="text-xl font-bold ">Eco Marketplace</h2>
         <div className="flex flex-row justify-between">
           <h4>Recommended</h4>
           <h6 className="text-xs">See All</h6>
         </div>
         <ul className="recommended-list">
-          <li className="recommended-list__item brocolli">
+          <li className="recommended-list__item rounded brocolli">
             <div className="absolute bottom-0 text-white pb-1 pl-2 w-full">
               <div className="flex flex-row justify-between mb-4">
                 <p className="text-white">Brocolli</p>
@@ -20,7 +20,7 @@ export class Recommended extends Component {
               <p className="absolute bottom-0 text-xs mb-1">Honeyfield Farms</p>
             </div>
           </li>
-          <li className="recommended-list__item bok-choy">
+          <li className="recommended-list__item rounded bok-choy">
             <div className="absolute bottom-0 text-white pb-1 pl-2 w-full">
               <div className="flex flex-row justify-between mb-4">
                 <p className="text-white">Bok Choy</p>
@@ -29,7 +29,7 @@ export class Recommended extends Component {
               <p className="absolute bottom-0 text-xs mb-1">Honeyfield Farms</p>
             </div>
           </li>
-          <li className="recommended-list__item carrots">
+          <li className="recommended-list__item rounded carrots">
             <div className="absolute bottom-0 text-white pb-1 pl-2 w-full">
               <div className="flex flex-row justify-between mb-4">
                 <p className="text-white">Carrots</p>
@@ -38,7 +38,7 @@ export class Recommended extends Component {
               <p className="absolute bottom-0 text-xs mb-1">Honeyfield Farms</p>
             </div>
           </li>
-          <li className="recommended-list__item potatoes">
+          <li className="recommended-list__item rounded potatoes">
             <div className="absolute bottom-0 text-white pb-1 pl-2 w-full">
               <div className="flex flex-row justify-between mb-4">
                 <p className="text-white">Potatoes</p>
@@ -47,7 +47,7 @@ export class Recommended extends Component {
               <p className="absolute bottom-0 text-xs mb-1">Honeyfield Farms</p>
             </div>
           </li>
-          <li className="recommended-list__item red-peppers">
+          <li className="recommended-list__item rounded red-peppers">
             <div className="absolute bottom-0 text-white pb-1 pl-2 w-full">
               <div className="flex flex-row justify-between mb-4">
                 <p className="text-white">Red Pepper</p>
